0101dc74b4c012ac2a368d93470fc25ed2a776b7
Your Name~clear <you@example.com>
2023-06-16 09:54:48 +0700

    baitapvenha

---
+++ buoi1/index.js
@@ -0,0 +1,27 @@
+/*
+ trăc nghiem 
+1.False
+2.true
+3.true
+4.3
+5.4
+6.1
+7.3
+8.4
+9.1
+10.2
+11.1
+12.2
+13.4
+*/
+//cau1
+function reverseStr(string) {
+    return string.split('').reverse().join('');
+}
+console.log (reverseStr("abcdef"))
+ //cau 2
+ /*function remove(arr) {
+    return arr.filter((item, index) => arr.indexOf(item) === index);
+  }
+console.log(remove(1,2,3,4,2,5,1,3,2,4,2,3))*/
+//cau34 chua lam duoc
