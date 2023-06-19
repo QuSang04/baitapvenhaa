fab340b97937d1f75f2ea14610bc55037f432d13
Your Name~clear <you@example.com>
2023-06-19 21:16:02 +0700

    fix readme

---
+++ lesson03-jpx-props/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentManagement from './component/StudentManagement/StudentManagement.js';
+function App() {
+  return (
+    
+    <>
+    <StudentManagement></StudentManagement>
+    </>
+  );
+}
+
+export default App;
+
